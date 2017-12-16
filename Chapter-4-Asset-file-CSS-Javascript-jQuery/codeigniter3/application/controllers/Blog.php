d54e7c10c90e9fec814b5ccbdfc8a1639ca67772
Tum <baleethai@gmail.com>
2017-12-16 15:28:20 +0700

    Chapter-4-Asset-file-CSS-Javascript-jQuery

--- Chapter-4-Asset-file-CSS-Javascript-jQuery/codeigniter3/application/controllers/Blog.php
+++ Chapter-4-Asset-file-CSS-Javascript-jQuery/codeigniter3/application/controllers/Blog.php
@@ -1,9 +1,22 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Blog extends CI_Controller {
-    public function index()
-    {
-        $this->load->helper('url');
-        $this->load->view('blogs/index');
+
+	public function index()
+	{
+		$this->load->view('blog_view');
+    }
+    public function detail(){
+        $this->load->view('blog_detail_view');
+    }
+    public function post(){
+        $this->load->view('blog_post_view');
+    }
+    public function category(){
+        $this->load->view('blog_cat_view');
+    }
+    public function comment(){
+ 
     }
 }
