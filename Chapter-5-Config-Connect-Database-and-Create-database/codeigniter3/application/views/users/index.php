0f2d22a18714ae06cd65b0e49dac8e6208dc7ae5
Tum <baleethai@gmail.com>
2017-12-16 17:02:49 +0700

    Update Chapter-6-Model

--- Chapter-5-Config-Connect-Database-and-Create-database/codeigniter3/application/views/users/index.php
+++ Chapter-5-Config-Connect-Database-and-Create-database/codeigniter3/application/views/users/index.php
@@ -1,142 +1,141 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="description" content="">
-        <meta name="author" content="">
-        
-        <title>Blog Home - Start Bootstrap Template</title>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="description" content="">
+		<meta name="author" content="">
+				
+		<title>Blog Home - Start Bootstrap Template</title>
 
-        <!-- Bootstrap core CSS -->
-        <link href="<?php echo base_url(); ?>template/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+		<!-- Bootstrap core CSS -->
+		<link href="<?php echo base_url(); ?>template/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-        <!-- Custom styles for this template -->
-        <link href="<?php echo base_url(); ?>template/css/blog-home.css" rel="stylesheet">
+		<!-- Custom styles for this template -->
+		<link href="<?php echo base_url(); ?>template/css/blog-home.css" rel="stylesheet">
+	</head>
 
-  </head>
+	<body>
 
-  <body>
+		<!-- Navigation -->
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+				<div class="container">
+				<a class="navbar-brand" href="#">Workshop Blog</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarResponsive">
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item active">
+							<a class="nav-link" href="<?php echo base_url();?>">Home
+								<span class="sr-only">(current)</span>
+							</a>
+						</li>           
+					</ul>
+				</div>
+			</div>
+		</nav>
 
-    <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-        <div class="container">
-        <a class="navbar-brand" href="#">Workshop Blog</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="<?php echo base_url();?>">Home
-                <span class="sr-only">(current)</span>
-              </a>
-            </li>           
-          </ul>
-        </div>
-      </div>
-    </nav>
+		<!-- Page Content -->
+		<div class="container">
 
-    <!-- Page Content -->
-    <div class="container">
+			<div class="row">
 
-      <div class="row">
+				<!-- Blog Entries Column -->
+				<div class="col-md-8">
+					<h3 class="my-4">List User
+					</h3>
 
-        <!-- Blog Entries Column -->
-        <div class="col-md-8">
-          <h3 class="my-4">List User
-          </h3>
+					<table class="table table-hover">
+						<thead>
+								<tr>
+								<th scope="col">#</th>
+								<th scope="col">Name</th>
+								<th scope="col">Email</th>
+								<th scope="col">Action</th>
+								</tr>
+						</thead>
+						<tbody>
+								<?php foreach($users as $user){ ?>
+								<tr>
+										<td scope="row"><?php echo $user->id; ?></td>
+										<td><?php echo $user->name; ?></td>
+										<td><?php echo $user->email; ?></td>
+										<td><a href="#">Edit</a> / <a href="#">Delete</a></td>
+								</tr>
+								<?php } ?>
+						</tbody>
+						</table>
 
-          <table class="table table-hover">
-            <thead>
-                <tr>
-                <th scope="col">#</th>
-                <th scope="col">Name</th>
-                <th scope="col">Email</th>
-                <th scope="col">Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach($users as $user){ ?>
-                <tr>
-                    <td scope="row"><?php echo $user->id; ?></td>
-                    <td><?php echo $user->name; ?></td>
-                    <td><?php echo $user->email; ?></td>
-                    <td><a href="#">Edit</a> / <a href="#">Delete</a></td>
-                </tr>
-                <?php } ?>
-            </tbody>
-            </table>
+				 
+				</div>
 
-         
-        </div>
+				<!-- Sidebar Widgets Column -->
+				<div class="col-md-4">
 
-        <!-- Sidebar Widgets Column -->
-        <div class="col-md-4">
+					<!-- Categories Widget -->
+					<div class="card my-4">
+						<h5 class="card-header">Categories</h5>
+						<div class="card-body">
+							<div class="row">
+								<div class="col-lg-6">
+									<ul class="list-unstyled mb-0">
+										<li>
+											<a href="#">Web Design</a>
+										</li>
+										<li>
+											<a href="#">HTML</a>
+										</li>
+										<li>
+											<a href="#">Freebies</a>
+										</li>
+									</ul>
+								</div>
+								<div class="col-lg-6">
+									<ul class="list-unstyled mb-0">
+										<li>
+											<a href="#">JavaScript</a>
+										</li>
+										<li>
+											<a href="#">CSS</a>
+										</li>
+										<li>
+											<a href="#">Tutorials</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
 
-          <!-- Categories Widget -->
-          <div class="card my-4">
-            <h5 class="card-header">Categories</h5>
-            <div class="card-body">
-              <div class="row">
-                <div class="col-lg-6">
-                  <ul class="list-unstyled mb-0">
-                    <li>
-                      <a href="#">Web Design</a>
-                    </li>
-                    <li>
-                      <a href="#">HTML</a>
-                    </li>
-                    <li>
-                      <a href="#">Freebies</a>
-                    </li>
-                  </ul>
-                </div>
-                <div class="col-lg-6">
-                  <ul class="list-unstyled mb-0">
-                    <li>
-                      <a href="#">JavaScript</a>
-                    </li>
-                    <li>
-                      <a href="#">CSS</a>
-                    </li>
-                    <li>
-                      <a href="#">Tutorials</a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
+					<!-- Side Widget -->
+					<div class="card my-4">
+						<h5 class="card-header">ราคา น้ำมัน</h5>
+						<div class="card-body">
+							You can put anything you want inside of these side widgets. They are easy to use, and feature the new Bootstrap 4 card containers!
+						</div>
+					</div>
 
-          <!-- Side Widget -->
-          <div class="card my-4">
-            <h5 class="card-header">ราคา น้ำมัน</h5>
-            <div class="card-body">
-              You can put anything you want inside of these side widgets. They are easy to use, and feature the new Bootstrap 4 card containers!
-            </div>
-          </div>
+				</div>
 
-        </div>
+			</div>
+			<!-- /.row -->
 
-      </div>
-      <!-- /.row -->
+		</div>
+		<!-- /.container -->
 
-    </div>
-    <!-- /.container -->
+		<!-- Footer -->
+		<footer class="py-5 bg-dark">
+			<div class="container">
+				<p class="m-0 text-center text-white">Copyright &copy; Your Website 2017</p>
+			</div>
+			<!-- /.container -->
+		</footer>
 
-    <!-- Footer -->
-    <footer class="py-5 bg-dark">
-      <div class="container">
-        <p class="m-0 text-center text-white">Copyright &copy; Your Website 2017</p>
-      </div>
-      <!-- /.container -->
-    </footer>
+		<!-- Bootstrap core JavaScript -->
+		<script src="<?php echo base_url('template/vendor/jquery/jquery.min.js'); ?>"></script>
+		<script src="<?php echo base_url('template/vendor/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
 
-    <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-  </body>
+	</body>
 
 </html>
